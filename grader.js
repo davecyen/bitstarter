18034988843041d0ed8d31a1d8dd5c17b09e0d81
Dave Yen <davecyen@gmail.com>
2013-07-19 06:53:41 +0000

    Updated grader.js and checks.json for part 3

--- grader.js
+++ grader.js
@@ -23,6 +23,8 @@ References:
    var fs = require('fs');
    var program = require('commander');
    var cheerio = require('cheerio');
+   var sys = require('util');
+   var rest = require('restler');
    var HTMLFILE_DEFAULT = "index.html";
    var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -37,6 +39,22 @@ References:
 
    var cheerioHtmlFile = function(htmlfile) {
        return cheerio.load(fs.readFileSync(htmlfile));
+   };
+
+   var readUrl = function(weburl) {
+       rest.get(weburl).on('complete', function(result){
+        if (result instanceof Error) {
+          sys.puts('Error ' + result.message);
+          this.retry(5000);
+        }
+        else {
+          sys.puts(result);
+        }
+       });
+   };
+
+   var cheerioUrl = function(url) {
+       return cheerio.load(readUrl(url));
    };
 
    var loadChecks = function(checksfile) {
@@ -64,6 +82,7 @@ References:
        program
            .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
            .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+           .option('-u, --url <external_url>', 'Path to external url')
            .parse(process.argv);
        var checkJson = checkHtmlFile(program.file, program.checks);
        var outJson = JSON.stringify(checkJson, null, 4);
